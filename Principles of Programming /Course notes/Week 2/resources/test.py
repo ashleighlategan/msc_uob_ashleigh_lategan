a4cf2f589900adc3dd64c34c85c42706c6ef08fa
Ashleigh Lategan <ashleigh.lategan27@gmail.com>
2026-05-07 21:22:13 +0100

    completed notebook

---
+++ Principles of Programming /Course notes/Week 2/resources/test.py
@@ -0,0 +1,8 @@
+print("This program will loop until you type quit")
+text = input("> ")
+
+while text != "quit":
+    print("That input is not the word quit")
+    text = input("> ")
+    
+print("Goodbye.")
