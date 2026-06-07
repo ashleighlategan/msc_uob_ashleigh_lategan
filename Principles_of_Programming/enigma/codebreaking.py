222615c6e91c3383c0fdac0f810fa21ef4851da6
Ashleigh Lategan <ashleigh.lategan27@gmail.com>
2026-06-07 17:33:15 +0100

    solved first code

--- Principles_of_Programming/enigma/codebreaking.py
+++ Principles_of_Programming/enigma/codebreaking.py
@@ -1,1 +1,18 @@
-from enigma import EnigmaMachine 
+from enigma import EnigmaMachine, REFLECTOR_NAMES
+
+crib = 'SECRETS'
+code_text = 'DMEXBMKYCVPNQBEDHXVPZGKMTFFBJRPJTLHLCHOTKOYXGGHZ'
+
+for reflector in REFLECTOR_NAMES:
+    
+    enigma_machine_1 = EnigmaMachine(
+        rotor_names = ["Beta","Gamma","V"],
+        reflector_name = reflector,
+        ring_settings= [4, 2, 14],
+        starting_positions = "MJM",
+        plugboard_pairs = ["KI", "XN", "FL"],
+        )
+    output = enigma_machine_1.encode_string(code_text)
+    if crib in output:
+        print(reflector, output)
+
