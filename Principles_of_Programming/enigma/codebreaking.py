1de940ca66a83ae82364ace0872c8cc5e02ff1f1
Ashleigh Lategan <ashleigh.lategan27@gmail.com>
2026-06-07 17:37:48 +0100

    updated to wrap in as a function

--- Principles_of_Programming/enigma/codebreaking.py
+++ Principles_of_Programming/enigma/codebreaking.py
@@ -1,18 +1,29 @@
 from enigma import EnigmaMachine, REFLECTOR_NAMES
 
-crib = 'SECRETS'
-code_text = 'DMEXBMKYCVPNQBEDHXVPZGKMTFFBJRPJTLHLCHOTKOYXGGHZ'
+def solution_code_1():
+    crib = 'SECRETS'
+    code_text = 'DMEXBMKYCVPNQBEDHXVPZGKMTFFBJRPJTLHLCHOTKOYXGGHZ'
 
-for reflector in REFLECTOR_NAMES:
+    for reflector in REFLECTOR_NAMES:
+        
+        enigma_machine = EnigmaMachine(
+            rotor_names = ["Beta","Gamma","V"],
+            reflector_name = reflector,
+            ring_settings= [4, 2, 14],
+            starting_positions = "MJM",
+            plugboard_pairs = ["KI", "XN", "FL"],
+            )
+        output = enigma_machine.encode_string(code_text)
+
+        if crib in output:
+            print(f"Code 1, uses Reflector: {reflector}, Decoded text is: {output}")
+
+if __name__ == "__main__":
+    solution_code_1()
+
+        
+        
     
-    enigma_machine_1 = EnigmaMachine(
-        rotor_names = ["Beta","Gamma","V"],
-        reflector_name = reflector,
-        ring_settings= [4, 2, 14],
-        starting_positions = "MJM",
-        plugboard_pairs = ["KI", "XN", "FL"],
-        )
-    output = enigma_machine_1.encode_string(code_text)
-    if crib in output:
-        print(reflector, output)
+
+
 
