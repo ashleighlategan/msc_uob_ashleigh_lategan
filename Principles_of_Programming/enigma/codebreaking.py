6c263ced2cc61862de865d26da6a820fe468a074
Ashleigh Lategan <ashleigh.lategan27@gmail.com>
2026-06-07 23:30:03 +0100

    completed code 4

--- Principles_of_Programming/enigma/codebreaking.py
+++ Principles_of_Programming/enigma/codebreaking.py
@@ -48,7 +48,7 @@ def solution_code_3():
     code_text = 'ABSKJAKKMRITTNYURBJFWQGRSGNNYJSDRYLAPQWIAGKJYEPCTAGDCTHLCDRZRFZHKNRSDLNPFPEBVESHPY'
     valid_rotor_names = ['Beta', 'Gamma', 'II', 'IV']
     valid_ring_settings = [2, 4, 6, 8, 20, 22, 24, 26]
-    crib_matches = []       # empty list to store any outputs that contain the crib
+    crib_matches = []       
 
     for rotors_used in permutations(valid_rotor_names, 3):
         for reflector_used in REFLECTOR_NAMES:
@@ -73,10 +73,54 @@ def solution_code_3():
               ring settings: {match['ring_settings']}, Decoded text: {match['output']}""")
     return crib_matches
 
+def solution_code_4():
+        crib = 'TUTOR'
+        code_text = 'SDNTVTPHRBNWTLMZTQKZGADDQYPFNHBPNHCQGBGMZPZLUAVGDQVYRBFYYEIXQWVTHXGNW' 
+        crib_matches = [] 
+        plugboard_pairs = 'WP RJ A? VF I? HN CG BS'.split()
+        known_pairs = [p for p in plugboard_pairs if '?' not in p]
+        used_letters = {c for p in plugboard_pairs for c in p if c != '?'}
+        scenario_2_pool = set(ALPHABET) - used_letters 
+        
+        # Scenario 1: I and A are connected to each other:
+        test_pairs = known_pairs + ['AI']
+        enigma_machine = EnigmaMachine(
+            rotor_names = ['V', 'III', 'IV'],
+            reflector_name = 'A',
+            ring_settings= [24, 12, 10],
+            starting_positions = "EMY",
+            plugboard_pairs = test_pairs,
+            )
+        output = enigma_machine.encode_string(code_text)
+        if crib in output:
+            crib_matches.append({"plugboard_pairs": test_pairs, "output": output})
+
+        # Scenario 2: I and A are not connected to each other:
+        for a_plug in scenario_2_pool:
+            for i_plug in scenario_2_pool - {a_plug}:
+                test_pairs = known_pairs + ['A' + a_plug] + ['I' + i_plug]
+                enigma_machine = EnigmaMachine(
+                    rotor_names = ['V', 'III', 'IV'],
+                    reflector_name = 'A',
+                    ring_settings= [24, 12, 10],
+                    starting_positions = "SWU",
+                    plugboard_pairs = test_pairs,
+                    )
+                output = enigma_machine.encode_string(code_text)
+                if crib in output:
+                    crib_matches.append({"plugboard_pairs": test_pairs, "output": output})
+    
+        for match in crib_matches:
+            print(f"Code 4 plugboard pairs: {match['plugboard_pairs']}, Decoded text: {match['output']}")
+        return crib_matches    
+        
+
+
 if __name__ == "__main__":
     solution_code_1()
     solution_code_2()
     solution_code_3()
+    solution_code_4()
 
 
         
