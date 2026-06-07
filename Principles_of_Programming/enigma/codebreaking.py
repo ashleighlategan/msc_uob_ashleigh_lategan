42dc5a633a3e4a165bf53973e040d639dade4447
Ashleigh Lategan <ashleigh.lategan27@gmail.com>
2026-06-07 18:41:36 +0100

    working on solution CODE 2

--- Principles_of_Programming/enigma/codebreaking.py
+++ Principles_of_Programming/enigma/codebreaking.py
@@ -1,4 +1,4 @@
-from enigma import EnigmaMachine, REFLECTOR_NAMES
+from enigma import EnigmaMachine, REFLECTOR_NAMES, ALPHABET
 
 def solution_code_1():
     crib = 'SECRETS'
@@ -19,10 +19,29 @@ def solution_code_1():
             print(f"Code 1, uses Reflector: {reflector}, Decoded text is: {output}")
 
 def solution_code_2():
-    
+    crib = 'UNIVERSITY'
+    code_text = 'CMFSUPKNCBMUYEQVVDYKLRQZTPUFHSWWAKTUGXMPAMYAFITXIJKMH'
+
+    for r_1 in ALPHABET:
+        for r_2 in ALPHABET:
+            for r_3 in ALPHABET:
+                enigma_machine = EnigmaMachine(
+                    rotor_names = ["Beta","I","III"],
+                    reflector_name = "B",
+                    ring_settings= [23, 2, 10],
+                    starting_positions = r_1 + r_2 + r_3,
+                    plugboard_pairs = ["VH", "PT", "ZG", "BJ", "EY", "FS"],
+                    )
+                output = enigma_machine.encode_string(code_text)
+                if crib in output:
+                    print(f"Code 2, uses rotor_positions: {r_1 + r_2 + r_3}, Decoded text is: {output}")
+                    return      # stop the loop if crib is found to be in the output
+
 
 if __name__ == "__main__":
     solution_code_1()
+    solution_code_2()
+
 
         
         
