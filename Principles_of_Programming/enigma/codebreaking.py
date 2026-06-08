fdba4576e0264262cd6fbbceffa2f7ad935ab4dc
Ashleigh Lategan <ashleigh.lategan27@gmail.com>
2026-06-08 23:53:14 +0100

    have a solution to code 5 but only hackily as it timesout need to review and optimize

--- Principles_of_Programming/enigma/codebreaking.py
+++ Principles_of_Programming/enigma/codebreaking.py
@@ -1,5 +1,6 @@
-from enigma import EnigmaMachine, REFLECTOR_NAMES, ALPHABET
-from itertools import permutations, product
+from enigma import EnigmaMachine, Reflector, REFLECTOR_NAMES, ALPHABET, ROTOR_WIRING, ALPHA_TO_IDX
+from itertools import permutations, product, combinations
+from math import comb
 
 def solution_code_1():
     crib = 'SECRETS'
@@ -9,7 +10,7 @@ def solution_code_1():
         
         enigma_machine = EnigmaMachine(
             rotor_names = ["Beta","Gamma","V"],
-            reflector_name = reflector,
+            reflector = Reflector(reflector),
             ring_settings= [4, 2, 14],
             starting_positions = "MJM",
             plugboard_pairs = ["KI", "XN", "FL"],
@@ -27,7 +28,7 @@ def solution_code_2():
     for r_1, r_2, r_3 in product(ALPHABET, repeat=3):
         enigma_machine = EnigmaMachine(
             rotor_names = ["Beta","I","III"],
-            reflector_name = "B",
+            reflector = Reflector("B"),
             ring_settings= [23, 2, 10],
             starting_positions = r_1 + r_2 + r_3,
             plugboard_pairs = ["VH", "PT", "ZG", "BJ", "EY", "FS"],
@@ -55,7 +56,7 @@ def solution_code_3():
             for ring_settings_used in product(valid_ring_settings, repeat=3):
                 enigma_machine = EnigmaMachine(
                     rotor_names = list(rotors_used),
-                    reflector_name = reflector_used,
+                    reflector = Reflector(reflector_used),
                     ring_settings= list(ring_settings_used),
                     starting_positions = "EMY",
                     plugboard_pairs = ["FH", "TS", "BE", "UQ", "KD", "AL"],
@@ -86,7 +87,7 @@ def solution_code_4():
         test_pairs = known_pairs + ['AI']
         enigma_machine = EnigmaMachine(
             rotor_names = ['V', 'III', 'IV'],
-            reflector_name = 'A',
+            reflector = Reflector('A'),
             ring_settings= [24, 12, 10],
             starting_positions = "EMY",
             plugboard_pairs = test_pairs,
@@ -101,7 +102,7 @@ def solution_code_4():
                 test_pairs = known_pairs + ['A' + a_plug] + ['I' + i_plug]
                 enigma_machine = EnigmaMachine(
                     rotor_names = ['V', 'III', 'IV'],
-                    reflector_name = 'A',
+                    reflector = Reflector('A'),
                     ring_settings= [24, 12, 10],
                     starting_positions = "SWU",
                     plugboard_pairs = test_pairs,
@@ -114,13 +115,72 @@ def solution_code_4():
             print(f"Code 4 plugboard pairs: {match['plugboard_pairs']}, Decoded text: {match['output']}")
         return crib_matches    
         
+def solution_code_5():
+    cribs = ['FACEBOOK', 'INSTAGRAM', 'TIKTOK', 'SNAPCHAT', 'PINTEREST', 'TWITTER' , 'REDDIT', 'YOUTUBE', 'WHATSAPP', 'LINKEDIN']
+    code_text = 'HWREISXLGTTBYVXRCWWJAKZDTVZWKBDJPVQYNEQIOTIFX' 
+    crib_matches = [] 
 
+    for reflector_name in REFLECTOR_NAMES:
+        wiring = ROTOR_WIRING[reflector_name]
+        pairs = [ (ALPHABET[i], wiring[i]) for i in range(26) if ALPHABET[i] < wiring[i]]
+        print(f"Reflector {reflector_name}: {len(pairs)} pairs: {pairs}")
+        break  # just check first reflector for now
+
+        for changed_pairs in combinations(pairs,4):
+            for first_choice in combinations(changed_pairs,2):
+                second_choice = [p for p in changed_pairs if p not in first_choice]
+                for swap_1 in range(2):
+                    (a1, a2) = first_choice[0]      # 1st pair e.g. ('C', 'M') where a1 = 'C' and a2 = 'M'
+                    (b1, b2) = first_choice[1]      # 2nd pair e.g. ('F', 'P') where a1 = 'F' and a2 = 'P'
+                    if swap_1 ==0:                  # becomes ('C','P'), ('F','M')
+                        new_pairs_1 = [(a1, b2), (b1, a2)]
+                    else:                           # becomes ('C','F'), ('M','P')
+                        new_pairs_1 = [(a1, b1), (a2, b2)]
+
+                    for swap_2 in range(2):
+                        (c1, c2) = second_choice[0]      # 1st pair e.g. ('B', 'R') where a1 = 'B' and a2 = 'R'
+                        (d1, d2) = second_choice[1]      # 2nd pair e.g. ('D', 'Z') where a1 = 'D' and a2 = 'Z'    
+                        if swap_2 ==0:                  # becomes ('B','Z'), ('D','R)
+                            new_pairs_2 = [(c1, d2), (d1, c2)]
+                        else:                           # becomes ('B','D'), ('R','Z')
+                            new_pairs_2 = [(c1, d1), (c2, d2)]
+                                
+                        wiring_list = list(wiring)
+                        for (x, y) in new_pairs_1 + new_pairs_2:
+                            wiring_list[ALPHA_TO_IDX[x]] = y
+                            wiring_list[ALPHA_TO_IDX[y]] = x
+                                
+                        updated_wiring = "".join(wiring_list)
+                        enigma_machine = EnigmaMachine(
+                            rotor_names = ['V', 'II', 'IV'],
+                            reflector = Reflector(reflector_name, updated_wiring),
+                            ring_settings= [6, 18, 7],
+                            starting_positions = "AJL",
+                            plugboard_pairs = ["UG", "IE", "PO", "NX", "WT"],
+                        )
+                        output = enigma_machine.encode_string(code_text)
+
+                        for crib in cribs:
+
+                            if crib in output:
+                                crib_matches.append({
+                                    'reflector': reflector_name,
+                                    'new_pairs_1': new_pairs_1,
+                                    'new_pairs_2': new_pairs_2,
+                                    'output': output
+                                })
+    for match in crib_matches:
+        print(f""" Code 5 Reflector: {match['reflector']}, output: {match['output']}
+              new_pairs_1: {match['new_pairs_1']}
+              new_pairs_2: {match['new_pairs_2']}""")
+    return crib_matches
 
 if __name__ == "__main__":
-    solution_code_1()
-    solution_code_2()
-    solution_code_3()
-    solution_code_4()
+    #solution_code_1()
+    #solution_code_2()
+    #solution_code_3()
+    #solution_code_4()
+    solution_code_5()
 
 
         
